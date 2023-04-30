5abf426f3cbe41711221d575b03aceb0391989da
SantiagoPachon77 <125226091+SantiagoPachon77@users.noreply.github.com>
2023-04-30 11:57:49 -0500

    Update m1_model_deployment.py

--- m1_model_deployment.py
+++ m1_model_deployment.py
@@ -2,6 +2,7 @@
 import pandas as pd
 import numpy as np
 from sklearn.ensemble import RandomForestRegressor
+from xgboost import XGBRegressor
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder, RobustScaler
 import joblib
 import sys
